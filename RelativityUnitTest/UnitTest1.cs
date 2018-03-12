8b85c39d52442968b0eac2275e08508306c8bd6d
Marius Utheim <marius_utheim@hotmail.com>
2018-03-12 21:01:50 +0100

    add unit test project

---
+++ RelativityUnitTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Relativity;
+
+namespace RelativityUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Random _rnd = new Random();
+
+        [TestMethod]
+        public void Observer_is_at_own_origin()
+        {
+            var observer = Observer.Default.Offset(_rnd.NextDouble(), _rnd.NextDouble(), _rnd.NextDouble());
+            Assert.IsTrue(observer.Origin.In(observer) == new Coordinate(0, 0));
+        }
+    }
+}
