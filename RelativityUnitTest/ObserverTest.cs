8642cb8a210f7497cdb21a20f78b28bedfd5579a
Marius Utheim <marius_utheim@hotmail.com>
2018-03-12 21:04:14 +0100

    rename UnitTest1 --> ObserverTest

--- RelativityUnitTest/ObserverTest.cs
+++ RelativityUnitTest/ObserverTest.cs
@@ -5,14 +5,14 @@ using Relativity;
 namespace RelativityUnitTest
 {
     [TestClass]
-    public class UnitTest1
+    public class ObserverTest
     {
         private Random _rnd = new Random();
 
         [TestMethod]
         public void Observer_is_at_own_origin()
         {
-            var observer = Observer.Default.Offset(_rnd.NextDouble(), _rnd.NextDouble(), _rnd.NextDouble());
+            var observer = Relativity.Observer.Default.Offset(_rnd.NextDouble(), _rnd.NextDouble(), _rnd.NextDouble());
             Assert.IsTrue(observer.Origin.In(observer) == new Coordinate(0, 0));
         }
     }
